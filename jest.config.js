632c6142c76379fb86a2aa064d42b778d8db2229
Nicole Quenamá <110855023+nicoleQuenama@users.noreply.github.com>
2026-06-17 19:31:37 -0400

    nuevos cambios

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   preset: 'jest-expo',
+  testEnvironment: "allure-jest/node",
+  testEnvironmentOptions:{
+    resultsDir:'allure-results'
+  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
   reporters: [
     'default',
-    ['allure-jest',{resultsDir: 'allure-results'
-    }]
   ]
 };
